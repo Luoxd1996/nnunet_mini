e70b2d6fc5d97ea10cdce6c0cb03f5083216def3
Xiangde Luo <19658103+Luoxd1996@users.noreply.github.com>
2023-02-20 10:36:41 +0800

    Update train_shell.sh

--- train_shell.sh
+++ train_shell.sh
@@ -1,6 +1,2 @@
-# python -u -m run_training 2d nnUNetTrainer Task001_WestChinaNPC 0 &
-# python -u -m run_training 2d nnUNetTrainer Task001_WestChinaNPC 1 &
-# python -u -m run_training 2d nnUNetTrainer Task001_WestChinaNPC 2 &
-# python -u -m run_training 2d nnUNetTrainer Task001_WestChinaNPC 3 &
-# python -u -m run_training 3d_fullres nnUNetTrainer Task002_ABDSeg all
+# Train a SwinUNETR on ABOD300 dataset.
 CUDA_VISIBLE_DEVICES=0 python -u -m run_training 3d_fullres nnUNetTrainer Task006_ABOD300 all
